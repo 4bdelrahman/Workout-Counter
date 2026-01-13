3c58ef56e0b029c4a2b05b361bde3bbeb0c5d1ac
4bdelrahman <Abdelrhmanahmed.myp1@gmail.com>
2026-01-13 10:16:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  import { getDatabase, ref, set, onValue, push, update, remove } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+  const firebaseConfig = {
+    databaseURL: "https://workouts-counter-default-rtdb.europe-west1.firebasedatabase.app/"
+    };
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const referenceInDB = ref(database , 'Workouts-num-db')
+ 
+  
+
+const incrementBtn = document.getElementById("increment-btn")
+const decrementBtn = document.getElementById("decrement-btn")
+const numberInputEl = document.getElementById("number-input-el")
+const addInputBtn = document.getElementById("add-input-btn")
+const workoutsEl = document.getElementById("workouts-el")
+
+
+
+ let numberOfWorkouts
+onValue(referenceInDB , function(snapshot) {
+    if (snapshot.exists()) {
+    const dataObject = snapshot.val()
+    const data = Object.values(dataObject)
+    numberOfWorkouts = data[0]
+    render()
+    }
+})
+
+incrementBtn.addEventListener("click" , function() {
+    numberOfWorkouts += 1
+    remove(referenceInDB)
+    push (referenceInDB , numberOfWorkouts)
+    render()
+})
+decrementBtn.addEventListener("click" , function() {
+    numberOfWorkouts -= 1
+    remove(referenceInDB)
+    push (referenceInDB , numberOfWorkouts)
+    render()
+})
+addInputBtn.addEventListener("click" , function () {
+    numberOfWorkouts =   Number(numberInputEl.value)
+    remove(referenceInDB)
+    push (referenceInDB , numberOfWorkouts)
+    render()
+})
+
+
+
+
+function render() {
+    workoutsEl.innerHTML = `<strong>${numberOfWorkouts}</strong> Workouts`
+}
